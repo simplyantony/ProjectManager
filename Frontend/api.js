f7009cd1b68e99c3a2549b2ec36407c7188a4294
Anthony Bryant <anthonygbryant2@gmail.com>
2026-04-10 16:33:00 -0400

    Update Frontend

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000/api'});
+const API = axios.create({baseURL: 'https://projectmanager-1-odhg.onrender.com/api'});
 
 export default API;
